96987a6cf3f1092e50b9a22e96594df1a0297357
= <pumaronal2016@gmail.com>
2024-10-05 12:57:14 -0400

    Se agrego Grafico de Lineas

--- src/components/Modal/ModalGrafico.jsx
+++ src/components/Modal/ModalGrafico.jsx
@@ -1,8 +1,46 @@
 import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+const etiquetas = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const datos = {
+  labels: etiquetas,
+  datasets: [
+    {
+      label: 'Mi primer conjunto de datos',
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1
+    }
+  ]
+};
+
 
 const ModalGrafico = ({ open, onClose }) => {
 
   if (!open) return null;
+
+//   const etiquetas = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+//   const datos = {
+//     labels: etiquetas,
+//     datasets: [
+//       {
+//         label: 'Mi primer conjunto de datos',
+//         data: [65, 59, 80, 81, 56, 55, 40],
+//         fill: false,
+//         borderColor: 'rgb(75, 192, 192)',
+//         tension: 0.1
+//       }
+//     ]
+//   };
+  
+//   const config = {
+//     type: 'line',
+//     data: datos,
+//   };
 
 
   const handleNuevoEncargadoEspecialidad = async () => {
@@ -16,7 +54,7 @@ const ModalGrafico = ({ open, onClose }) => {
 
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 z-10">
+    <div className="fixed inset-0 z-10">
       <form
         className="fixed top-0 m-5 right-0 max-w-lg w-11/12 max-h-[90vh] bg-white shadow-2xl rounded-2xl p-5"
       >
@@ -33,13 +71,14 @@ const ModalGrafico = ({ open, onClose }) => {
           Modal Modal Grafico
         </h2> 
 
-        <div className="flex justify-center">
-          <button
-            type="submit"
-            className="bg-green-600 hover:bg-green-700 font-semibold mt-5 text-white py-2 px-5 rounded-xl"
-          >
-            Hola
-          </button>
+        <div className="flex flex-col w-full justify-center">
+          
+
+          <div className="mt-5">
+            <Line data={datos} />
+          </div>
+
+
         </div>
       </form>
     </div>
